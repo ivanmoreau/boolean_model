ff68606a38df6fcff9c8d11c6fc041f7b4754e44
Ivan Molina Rebolledo <ivanmolinarebolledo@gmail.com>
2022-02-16 18:13:57 -0600

    aaa

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 
-import Boolean_model (someFunc)
+import BooleanModel (someFunc)
 
 
 main :: IO ()
---
+++ src/BooleanModel.hs
@@ -0,0 +1,58 @@
+{- |
+Copyright: (c) 2022 Iván Molina Rebolledo
+SPDX-License-Identifier: GPL-3.0-only
+Maintainer: Iván Molina Rebolledo <ivanmolinarebolledo@gmail.com>
+
+See README for more info
+-}
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module BooleanModel
+       ( someFunc
+       ) where
+import Data.List (nub, delete)
+import Data.Text (Text, pack, replace, splitOn)
+import Data.Map (Map, fromList, insert, insertWith, (!), delete)
+import qualified Data.Map as Dm
+import Data.Text.IO as DT (readFile)
+
+
+rone :: [Text]
+rone = ["$user tweeted ", " $ht", " $user"]
+
+fixWords :: Text -> [Text] -> Text
+fixWords xs []     = xs
+fixWords xs (l:ls) = fixWords (replace l (pack "") xs) ls
+
+getWords :: Text -> Map Text (Map Int Bool)
+getWords l = fromList (map (\x -> (x, fromList [(0, False)])) (Data.List.delete "" (nub (splitOn " " (replace "\n" " " l)))) )
+
+emptymatrix :: Map Text (Map Int Bool) -> Int -> Map Text (Map Int Bool)
+emptymatrix m c = Dm.map (\o -> fromList (map (\x -> (x, False)) [0..c])) m
+
+aa :: Map Text (Map Int Bool) -> Text -> Int -> Map Text (Map Int Bool)
+aa m t i = let r = m ! t in
+  insert t (insert i True r) m
+
+--mLine :: 
+byLine :: [Text] -> Map Text (Map Int Bool) -> Int -> Map Text (Map Int Bool)
+byLine [] m _ = m
+byLine (l:ls) m c = byLine ls (foldr (\x y -> aa y x c) m (nub (splitOn " " l))) (c + 1)
+
+genMatrix :: Text
+  -> Map Text (Map Int Bool)
+genMatrix t = let ff = fixWords t rone 
+                  gw = getWords ff
+                  ts = (splitOn "\n" ff)  
+                  em = emptymatrix gw (length ts) in
+  byLine ts em 0 
+
+
+readTest name = do
+  c <- DT.readFile name
+  let r = genMatrix c
+  return r
+
+someFunc :: IO ()
+someFunc = putStrLn ("someFunc" :: String)
